e59e1d0e8c51512f5febdcf3522c1b8861b0a5d5
UzixLS <uzix.ls@gmail.com>
2021-05-29 09:48:20 +0300

    add source code first version

---
+++ RainMod_HideMap/HideMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RWCustom;
+
+public class HideMap
+{
+    public static void Patch()
+    {
+        On.HUD.Map.Update += Map_Update;
+    }
+
+    private static void Map_Update(On.HUD.Map.orig_Update orig, HUD.Map self)
+    {
+        orig.Invoke(self);
+        self.visible = false;
+    }
+}
---
+++ RainMod_HideMap/RainMod_HideMap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Partiality.Modloader;
+using UnityEngine;
+
+public class RainMod_HideMap : PartialityMod
+{
+    public RainMod_HideMap()
+    {
+        this.ModID = "RainMod_HideMap";
+        this.Version = "0100";
+        this.author = "Uzix";
+    }
+
+    public static RainMod_HideMap instance;
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        //Hooks go here
+        HideMap.Patch();
+    }
+}
